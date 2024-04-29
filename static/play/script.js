b2dca2f5760d7eb61ca7fc17f74dba30bea20516
takechi-scratch <takechi.scratch@gmail.com>
2024-04-29 19:35:00 +0900

    スコア項目増、位置調整

--- static/play/script.js
+++ static/play/script.js
@@ -1,7 +1,8 @@
 let startD
 let typingStr
 let typingNumber
-console.log("コンソールへようこそ！")
+let miss
+console.log("コンソールへようこそ！");
 document.querySelector(".prepare").innerText = "準備中…";
 
 const url = new URL(window.location.href);
@@ -50,13 +51,18 @@ function main(){
     startD = Date.now();
     typingStr = "reta-pakkudegenkinokurehasubetesagidesu";
     typingNumber = 0;
+    miss = 0;
 
     document.querySelector(".prepare").remove();
 
     const interval = setInterval(function() {
-        document.querySelector(".time").innerText = Math.round((Date.now() - startD) / 10) / 100;
+        document.querySelector(".time").innerText = Math.round(player.getCurrentTime() * 100) / 100;
         document.querySelector(".finishedStr").innerText = typingStr.substring(0, typingNumber);
         document.querySelector(".yetStr").innerText = typingStr.substring(typingNumber, typingStr.length);
+
+        document.querySelector(".corrects").innerText = typingNumber;
+        document.querySelector(".miss").innerText = miss;
+
         if (typingNumber == typingStr.length) {
             clearInterval(interval)
             const sound = new Audio("/static/sound/next.mp3");
@@ -82,6 +88,7 @@ document.addEventListener("keydown", (event) => {
             const sound = new Audio("/static/sound/type.wav");
             sound.play();
         } else {
+            miss++;
             const sound = new Audio("/static/sound/miss.wav");
             sound.play();
         }
